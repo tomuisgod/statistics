35000f753f519bd1bfaad8f36d3a962780e2702e
Tomáš Lovrant <66744640+tomuisgod@users.noreply.github.com>
2025-05-21 09:44:19 +0200

    Update Untitled.R

--- Untitled.R
+++ Untitled.R
@@ -1,6 +1,7 @@
 library(readxl)
 library(ggplot2)
 library(stargazer)
+library(moments)
 
 data_str <- as.data.frame(read_xlsx("/Users/macbookpro/haha/oil.xlsx"))
 
@@ -13,6 +14,7 @@ plot(data_str$oil, data_str$gold,
      main = "Oil vs Gold",
      xlab = "Oil",
      ylab = "Gold")
+print("preslo")
 
 ggplot(data = data_str, aes(x=t, y=oil, group=1)) +
   geom_line(color = "black")+
@@ -23,8 +25,25 @@ ggplot(data = data_str, aes(x=t, y=oil, group=1)) +
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank()) +
   ggtitle("Oil price")
+print("preslo")
 
 stargazer(data_str, type = "text", title = "Descriptive statistics", 
-          digit = 3, flip = true)
+          digit = 3, flip=TRUE)
+print("preslo")
 
 apply(data_str[,-1], 2, sd)
+apply(data_str[,-1], 2, mean)
+apply(data_str[,-1], 2, var)
+
+# finding ?? for a single var
+sd_value <- sd(data_str$oil)
+mean_value <- mean(data_str$oil)
+med <- median(data_str$oil)
+
+# histrogram
+hist(data_str$oil)
+
+# shape of distribution
+apply(data_str[,-1], 2, skewness)
+apply(data_str[,-1], 2, kurtosis)
+print("preslo")
